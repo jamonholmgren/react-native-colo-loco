f2fa4726ad3e0075bbca86464b019718472892c0
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-11-21 05:11:25 +0200

    Test install-colo-loco script

--- test/helpers.js
+++ test/helpers.js
@@ -1,15 +1,34 @@
 const path = require("path")
 const os = require("os")
 const fs = require("fs-extra")
-const { exec } = require("child_process")
-const { promisify } = require("util")
-
-const run = promisify(exec)
+const { spawn } = require("child_process")
 
 const COLO_LOCO = path.join(__dirname, "../scripts/install-colo-loco")
 
-function runColoLoco(args) {
-  return run(`node ${COLO_LOCO} ${args}`)
+function runColoLoco({ appName, sourceFolder, packageName } = {}, args = []) {
+  return new Promise((resolve) => {
+    const cp = spawn("node", [COLO_LOCO, ...args])
+
+    let output = ""
+    cp.stdout.on("data", (data) => {
+      output += data.toString()
+      console.log(data.toString())
+    })
+
+    let error = ""
+    cp.stderr.on("data", (data) => {
+      console.log(data.toString())
+      error += data.toString()
+    })
+
+    cp.on("close", (code) => {
+      resolve({ output, error, code })
+    })
+
+    cp.stdin.write(appName + "\n")
+    cp.stdin.write(sourceFolder + "\n")
+    cp.stdin.write(packageName + "\n")
+  })
 }
 
 async function createTempDir() {
--- test/install-colo-loco.test.js
+++ test/install-colo-loco.test.js
@@ -2,6 +2,11 @@ const fs = require("fs-extra")
 
 const { runColoLoco, createTempDir } = require("./helpers")
 
+const APP_NAME = "TestApp"
+const SOURCE_FOLDER = "src"
+const PACKAGE_NAME = "com.testapp"
+const ANDROID_PATH = "android/app/src/main/java/com/testapp"
+
 const originalDir = process.cwd()
 let tempDir
 
@@ -16,8 +21,40 @@ afterEach(async () => {
 })
 
 describe("Checking for install-colo-loco. 🤪", () => {
-  test("install-colo-loco", async () => {
-    const { stdout } = await runColoLoco("--defaults")
-    // TODO: test script
+  it("installs correctly", async () => {
+    const { output, error, code } = await runColoLoco({
+      appName: APP_NAME,
+      sourceFolder: SOURCE_FOLDER,
+      packageName: PACKAGE_NAME,
+    })
+
+    expect(code).toBe(0)
+    expect(error).toBe("")
+
+    // uses the right defaults (app name & source folder)
+    expect(output).toContain(APP_NAME)
+    expect(output).toContain(SOURCE_FOLDER)
+
+    // iOS set up is correct
+    // Podfile
+    const podfile = await fs.readFile(`${tempDir}/ios/Podfile`, "utf8")
+    expect(podfile).toContain("react-native-colo-loco/scripts/ios.rb")
+    expect(podfile).toContain("link_colocated_native_files")
+
+    // Android set up is correct
+    // settings.gradle
+    const settingsGradle = await fs.readFile(`${tempDir}/android/settings.gradle`, "utf8")
+    expect(settingsGradle).toContain("react-native-colo-loco/scripts/android.groovy")
+    expect(settingsGradle).toContain("linkColocatedNativeFiles")
+    expect(settingsGradle).toContain(PACKAGE_NAME)
+
+    // AppPackage.java
+    const appPackage = await fs.readFile(`${tempDir}/${ANDROID_PATH}/${APP_NAME}Package.java`, "utf8")
+    expect(appPackage).toContain("modules.addAll(ColoLoco.colocatedModules(reactContext))")
+    expect(appPackage).toContain("modules.addAll(ColoLoco.colocatedViewManagers(reactContext))")
+
+    // MainApplication.java
+    const mainApplication = await fs.readFile(`${tempDir}/${ANDROID_PATH}/MainApplication.java`, "utf8")
+    expect(mainApplication).toContain(`packages.add(new ${APP_NAME}Package())`)
   })
 })
